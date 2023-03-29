8deaf5f7c053ad909c8ca2b5043d533525fe87bd
Celia Mainar <75754618+celiia01@users.noreply.github.com>
2023-03-29 13:30:44 +0200

    Except choose velocity

--- test_sensors/src/readOdomLaser.py
+++ test_sensors/src/readOdomLaser.py
@@ -10,12 +10,16 @@ import time
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TwistStamped, Twist
-from multiprocessing import Lock
+from multiprocessing import Lock, Process
 
 
 class TestNode:
 	def __init__(self):
 		self.lidar_sub = message_filters.Subscriber('base_scan', LaserScan)
+		# figure = plt.figure(1)
+		# ax = figure.add_subplot(111)
+		# plt.show(block=False)
+		# plt.pause(0.01)
 		self.teleop_sub = rospy.Subscriber('teleop_vel', Twist, self.callback_teleop, queue_size=10)
 		self.odom_sub = message_filters.Subscriber('odom', Odometry)
 		self.desired_vel_sub = message_filters.Subscriber('desired_vel', TwistStamped)
@@ -29,6 +33,8 @@ class TestNode:
 		self.trayectoria_top = False
 		self.trayectoria_bottom = False
 		self.timeCall = 0.1
+		self.matrix = np.zeros((70,360))
+
 	
 	def callback_teleop(self, msg: Twist):
 		vel_stamped = TwistStamped()
@@ -94,11 +100,10 @@ class TestNode:
 		self.lock.release()
 		if (v > 0 and v < 0.001) or v == 0:
 			distance = 30.0
-			#print("giro")
 			return distance, distance
-		elif abs(w) > 0 and abs(w) < 0.0001:
+		elif abs(w) > 0 and abs(w) < 0.0001 or w == 0:
 			distance = np.amin(middle)
-			#print("distance middle: ", np.amin(middle))
+			# print("distance middle: ", np.amin(middle))
 			return distance - v * dt, distance 
 		elif w > 0.0001:
 			distance = np.amin(middle) if np.amin(middle) < np.amin(bottom) else np.amin(bottom)
@@ -109,9 +114,6 @@ class TestNode:
 
 		return distance - (v/w * (w * dt)), distance
 	
-	def acelerationsForBreak(self, distance, distance0, v):
-		acV = 2 * (distance0 - distance - v * self.timeCall) / self.timeCall ** 2
-		return acV if abs(acV) > 0.001 else 0
 	# OPTIMIZAR BUCLE!!!!
 	def velocities(self, middle: np.array, top: np.array, bottom: np.array, 
 		vAct: float, wAct: float, vMin: float, vMax: float, wMin: float, wMax: float):
@@ -134,74 +136,67 @@ class TestNode:
 
 
 		for v in np.arange(vMax, vMin, -vChange):
-			v = 0 if v > 0 and v < 0.01 else v
+			v = 0 if v > 0 and v < 0.01 else round(v, 2)
 			for w in np.arange(wMin, wMax, wChange):
 				
 				#print("v: ", v, ", w: ", w)
-				w = 0 if abs(w) < 0.001 and abs(w) > 0 else w
+				w = 0 if abs(w) < 0.001 and abs(w) > 0 else round(w,3)
 				distance, distance0 = TestNode.dist(self, v, w, middle,top,bottom)
-				#print(distance, ", ", distance0)
+				distance -= 0.15
+				distance = 0 if distance < 0 else distance
+				# print("v: ", v, " w: ",w, " distance: ", distance)
 
-				# Accelerations for breakage??
-				acelBreakV = TestNode.acelerationsForBreak(self,distance, distance0, v)
 				#print(acelBreakV)
 
 				limitV = math.sqrt(2.0 * distance * aceleMaxV)
 				limitW = math.sqrt(2.0 * distance * aceleMaxW)
-			
+
+				if (v > limitV or w > limitW):
+					Vr1[i][j] = 1
+				if (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp):
+					Vr1[i][j] = 2
 				if (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp and v <= limitV and w <= limitW):
 					Vr.append([v,w])
-					Vr1[i][j] = 1
+					Vr1[i][j] = 3
 				j += 1
 			j = 0
 			i += 1
-
+			Vr1[0][1] = 1
+			Vr1[0][2] = 2
+			Vr1[0][3] = 3
+		self.matrix = Vr1
 		t2 = time.clock_gettime(time.CLOCK_REALTIME) - t1
 		return Vr, Vr1
-	"""	
-	# Admissible Velocities
-	def velocityAdmissible(self, middle: np.array, top: np.array, bottom: np.array, 
-			vA):
-		
-		Va = []
-
-		for i in vA:
-			for v in i:	
-				#print(v)
-				distance = TestNode.dist(self, v[0],v[1],middle,top,bottom)
-				# Accelerations for breakage??
-				aceleMaxV = 0.7
-				aceleMaxW = math.pi
-
-				limitV = math.sqrt(2.0 * distance * aceleMaxV)
-				limitW = math.sqrt(2.0 * distance * aceleMaxW)
-
-				if (v[0] <= limitV and v[1] <= limitW):
-					Va.append([v[0],v[1]])
-		
-		return Va
 	
-	def velocityDynamic(self, vAct: float, wAct: float, 
-		     vMin: float, vMax: float, wMin: float, wMax: float ):
-		# Specifications of the robot
-		aceleMaxV = 0.7
-		aceleMaxW = math.pi
+	def startPlot(self):
+		self.p = Process(target=self.plotDWA(), args=())
+		self.p.start()
 
-		Vd = []
+	def plotDWA(self):
+		fig, ax = plt.subplots()
 
-		for v in np.arange(vMin, vMax, 0.01):
-			for w in np.arange(wMin, wMax, math.pi/180.0):
+		y = np.arange(0.8,0, -0.1)
+		y = np.round(y, 2)
+		x = np.arange(-np.pi-(2*np.pi/7), np.pi+(2*np.pi/7), (2*np.pi/7))
+		x = np.round(x, 3)
 
-				limitVLow = vAct - aceleMaxV * self.timeCall
-				limitVUp = vAct + aceleMaxV * self.timeCall
-				limitWLow = wAct - aceleMaxW * self.timeCall
-				limitWUp = wAct + aceleMaxW * self.timeCall
+		while(True):
+			ax.cla()
+			ax.set_title("DWA")
 
-				if (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp):
-					Vd.append([v,w])
+			self.lock.acquire()
+			ax.matshow(self.matrix)
+			self.lock.release()
 
-		return Vd
-		"""
+			ax.set_xticklabels(x)
+			ax.set_yticklabels(y)
+
+			fig.show()
+			plt.pause(0.1)
+
+	def searchVelocities(self, desired_v: float, desired_w: float, velocities):
+
+		pass
 		
 
 	def callback(self, scan: LaserScan, odom: Odometry, desired_vel: TwistStamped):
@@ -210,11 +205,11 @@ class TestNode:
 		# Desired Velocities
 		desired_v = desired_vel.twist.linear.x																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
 		desired_w = desired_vel.twist.angular.z
-		# Actual Velocities
+		# Actual Velocities (deseada)
 		vAct = odom.twist.twist.linear.x if odom.twist.twist.linear.x > 0.0 or odom.twist.twist.linear.x < 0.0 else desired_v
 		wAct = odom.twist.twist.angular.z if odom.twist.twist.angular.z > 0.0 or odom.twist.twist.angular.z < 0.0 else desired_w
 		
-		distance = 0.5
+		distance = 0.65
 		scan_ranges = np.array(scan.ranges)
 		middle = np.array(scan_ranges[478:600])
 		top = np.array(scan_ranges[601:])
@@ -227,27 +222,21 @@ class TestNode:
 		wMax = math.pi
 
 
-		Vs = np.array([[0 for w in np.arange(wMin, wMax, math.pi/180.0)] for v in np.arange(vMin, vMax, 0.01)])
+		# Vs = np.array([[0 for w in np.arange(wMin, wMax, math.pi/180.0)] for v in np.arange(vMin, vMax, 0.01)])
 		
 
 		_,Vr = TestNode.velocities(self, middle, top, bottom, vAct, wAct, vMin, vMax, wMin, wMax)
-		
-		# CODE inspirated from geeksgorgeeks.org/matplot-pyplot-matshow-in-python/
-		fig = plt.figure()
-		ax = fig.add_subplot(111)
 
-		ax.matshow(Vr)
+		self.matrix = Vr
 
-		plt.show()
-		plt.close('all')
+		# v, w = TestNode.skipObstacle(self,scan_ranges, middle, top, bottom, desired_v, desired_w, distance)
 		
 		#print(middle)
 
-		#print(len(scan.ranges), desired_v)																																																						
-		v = desired_v
+		#print(len(scan.ranges), desired_v)																																																					
+		
+		v  = desired_v
 		w = desired_w
-		
-		
 		self.send_vel(v,w)
 		#rospy.loginfo(rospy.get_caller_id() + "Min range %f", np.minimum(scan_ranges))
 
@@ -256,7 +245,11 @@ def main():
 
     test_node = TestNode()
 
+    test_node.startPlot()
+    
     rospy.spin() 
+    
+	
 
 if __name__ == "__main__":
     main()
