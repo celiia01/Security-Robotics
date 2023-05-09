00ccba724e39e90f4015f572fd356600b761d140
Celia Mainar <75754618+celiia01@users.noreply.github.com>
2023-05-09 09:28:49 +0200

    Polar coordinates

--- test_sensors/src/readOdomLaser.py
+++ test_sensors/src/readOdomLaser.py
@@ -14,12 +14,23 @@ from multiprocessing import Lock, Process
 
 
 class TestNode:
+	ANGLE_VISION = 270
+
+	FORBIDEN = 1
+	DYNAMIC_WINDOW = 2
+	FEASIBLE = 3
+	ACTUAL = 4
+
+	#Maximun Accelerations
+	AMAXV = 0.7
+	AMAXW = math.pi
+
+	MAXDISTANCE = AMAXW/2
+
+	SIZEROBOT = 0.15
+
 	def __init__(self):
 		self.lidar_sub = message_filters.Subscriber('base_scan', LaserScan)
-		# figure = plt.figure(1)
-		# ax = figure.add_subplot(111)
-		# plt.show(block=False)
-		# plt.pause(0.01)
 		self.teleop_sub = rospy.Subscriber('teleop_vel', Twist, self.callback_teleop, queue_size=10)
 		self.odom_sub = message_filters.Subscriber('odom', Odometry)
 		self.desired_vel_sub = message_filters.Subscriber('desired_vel', TwistStamped)
@@ -34,6 +45,9 @@ class TestNode:
 		self.trayectoria_bottom = False
 		self.timeCall = 0.1
 		self.matrix = np.zeros((70,360))
+		self.locRobot = np.array([-4.5, -6, 0])
+		self.locObstacles = np.array([[-3.5, -6, 0]])
+		self.sizeObstacles = np.array([[0.5, 0.5, 0.5]])
 
 	
 	def callback_teleop(self, msg: Twist):
@@ -47,6 +61,16 @@ class TestNode:
 		self.vel_msg.angular.z = w																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																					
 		self.vel_pub.publish(self.vel_msg)
 
+	def hom(self,x: np.array):
+		return np.array([
+			[np.cos(x[2]), -np.sin(x[2]), x[0]],
+			[np.sin(x[2]),  np.cos(x[2]), x[1]],
+			[			0,			   0,    1]
+		])
+
+	def loc(self,mat: np.array):
+		return np.array([mat[0][2], mat[1][2], math.atan2(mat[1][0], mat[0][0])])
+ 
 	def skipObstacle(self, scan_ranges, middle, top, bottom, desired_v, desired_w, distance):
 		self.lock.acquire()
 		# NO HA INICIADO NINGUNA TRAYECTORIA PARA ESQUIVAR
@@ -92,81 +116,157 @@ class TestNode:
 
 		return v,w
 	
-	# Minimun Distance from the robot to the most proximate obstacle
-	def dist(self, v: float, w: float, 
-	  middle: np.array, top: np.array, bottom: np.array):
+	#Next Position of the robot in the world with the velocities v and w
+	def nextPosition(self, v: float, w: float):
 		self.lock.acquire()
 		dt = self.timeCall
 		self.lock.release()
-		if (v > 0 and v < 0.001) or v == 0:
-			distance = 30.0
-			return distance, distance
-		elif abs(w) > 0 and abs(w) < 0.0001 or w == 0:
-			distance = np.amin(middle)
-			# print("distance middle: ", np.amin(middle))
-			return distance - v * dt, distance 
-		elif w > 0.0001:
-			distance = np.amin(middle) if np.amin(middle) < np.amin(bottom) else np.amin(bottom)
-			#print("middle: ", np.amin(middle), ", bottom: ", np.amin(bottom))
+		newX, newY, newTh = 0.0, 0.0, 0.0
+
+		if abs(w) > 0.001:
+			newX = self.locRobot[0] + (v / w) * (np.sin(self.locRobot[2] + w * dt) - np.sin(self.locRobot[2]))
+			newY = self.locRobot[1] + (v / w) * (np.cos(self.locRobot[2] + w * dt) - np.cos(self.locRobot[2]))
+			newTh = self.locRobot[2] + w * dt
 		else:
-			distance = np.amin(middle) if np.amin(middle) < np.amin(top) else np.amin(top)
-			#print("middle: ", np.amin(middle), ", top: ", np.amin(top))
-
-		return distance - (v/w * (w * dt)), distance
+			newX = self.locRobot[0] + dt * v * np.cos(self.locRobot[2])
+			newY = self.locRobot[1] + dt * v * np.sin(self.locRobot[2])
+			newTh = self.locRobot[2] + w * dt
+		return np.array([newX, newY, newTh])
 	
-	# OPTIMIZAR BUCLE!!!!
-	def velocities(self, middle: np.array, top: np.array, bottom: np.array, 
-		vAct: float, wAct: float, vMin: float, vMax: float, wMin: float, wMax: float):
-		aceleMaxV = 0.7
-		aceleMaxW = math.pi
-		vChange = 0.01
-		wChange = math.pi /180.0
+	def polar2Cartesian(self, distance: np.array):
+		return np.array([distance[0] * np.cos(distance[1]), distance[0] * np.sin(distance[1]), distance[1]])
+	
+	# Minimun Distance from the robot to the most proximate obstacle
+	def dist(self, v: float, w: float, polarObstacle: np.array):
+		obstacle = self.polar2Cartesian(polarObstacle)
 
-		Vr = []
-		x = int((vMax - vMin) / vChange)
-		y = int((wMax - wMin) / wChange)
-		Vr1 = np.zeros([x,y])
+		self.lock.acquire()
+		pos1 = self.locRobot
+		self.lock.release()
+
+		pos2 = self.nextPosition(v, w)
+
+
+		#Positions of the robot and the obstacle in the world
+		wTR2 = self.hom(pos2)
+		wTR1 = self.hom(pos1)
+		r1TO = self.hom(obstacle)
+
+		#Position relative of the obstacle
+		r2TO = np.linalg.inv(wTR2) @ wTR1 @ r1TO
+
+
+		rXO = self.loc(r2TO)
+
+		if rXO[1] == 0 and rXO[0] != 0:
+			return rXO[0], 1
+		elif rXO[0] == 0 and rXO[1] != 0:
+			return rXO[1], 1
+		else:
+			radius = (rXO[0]**2 + rXO[1]**2)/ (2 * rXO[1])
+			theta = math.atan2(2 * rXO[0] * rXO[1], rXO[0]**2 - rXO[1]**2)
+
+			return radius * theta, 1 if radius > 0 else -1
+
+
+		
+	
+
+	def velocities(self, vAct: float, wAct: float, vMin: float, vMax: float, wMin: float, wMax: float, scan_sub: np.array):
+		vChange = 0.05
+		wChange = 5 * math.pi / 180.0
+
+		x = int(round((vMax - vMin) / vChange, 0)) + 1
+		y = int(round((wMax - wMin) / wChange, 0)) + 1
+		dWA = np.zeros([x,y])
 		t1 = time.clock_gettime(time.CLOCK_REALTIME)
 		i = 0
 		j = 0
-		limitVLow = vAct - aceleMaxV * self.timeCall
-		limitVUp = vAct + aceleMaxV * self.timeCall
-		limitWLow = wAct - aceleMaxW * self.timeCall
-		limitWUp = wAct + aceleMaxW * self.timeCall
 
+		# Dynamically admissible velocities
+		limitVLow = vAct - self.AMAXV * self.timeCall
+		limitVUp = vAct + self.AMAXV * self.timeCall
+		limitWLow = wAct - self.AMAXW * self.timeCall
+		limitWUp = wAct + self.AMAXW * self.timeCall
+
+		obstacle = False
 
 		for v in np.arange(vMax, vMin, -vChange):
 			v = 0 if v > 0 and v < 0.01 else round(v, 2)
-			for w in np.arange(wMin, wMax, wChange):
-				
-				#print("v: ", v, ", w: ", w)
+			for w in np.arange(wMin, wMax + wChange, wChange):
 				w = 0 if abs(w) < 0.001 and abs(w) > 0 else round(w,3)
-				distance, distance0 = TestNode.dist(self, v, w, middle,top,bottom)
-				distance -= 0.15
-				distance = 0 if distance < 0 else distance
-				# print("v: ", v, " w: ",w, " distance: ", distance)
+				# Actual Velocity
+				if (v == vAct and w == wAct):
+					dWA[i][j] = self.ACTUAL
+				obstacle = False
+				for d in scan_sub:
+					# Maximum distance that limitW can be lower than w, the maximum distance thar limitV can be lower than v
+					# is minor.
+					if d[0] < self.MAXDISTANCE:
+						obstacle = True
+						distance, sign = TestNode.dist(self, v, w, d)
+						distance = distance - self.SIZEROBOT
 
-				#print(acelBreakV)
+						if distance < 0:
+							distance = 0.0
 
-				limitV = math.sqrt(2.0 * distance * aceleMaxV)
-				limitW = math.sqrt(2.0 * distance * aceleMaxW)
+						# Admissible velocities
+						# v <= (2 * distance(v, w) * vb ) ^ 1/2
+						# w <= (2 * distance(v,w) * wb ) ^ 1/2
 
-				if (v > limitV or w > limitW):
-					Vr1[i][j] = 1
-				if (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp):
-					Vr1[i][j] = 2
-				if (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp and v <= limitV and w <= limitW):
-					Vr.append([v,w])
-					Vr1[i][j] = 3
+						limitV = math.sqrt(2.0 * distance * self.AMAXV)
+						limitW = math.sqrt(2.0 * distance * self.AMAXW) * sign
+
+						wAdmissible = True
+						if limitW >= 0 and w >= 0:
+							wAdmissible = w <= limitW
+						elif limitW < 0 and w < 0:
+							wAdmissible = w >= limitW
+
+
+						# Feasible velocities
+						if (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp and v <= limitV and wAdmissible and dWA[i][j]!= self.ACTUAL and dWA[i][j] != self.FORBIDEN):
+							dWA[i][j] = self.FEASIBLE
+						# Dynamic Window
+						elif (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp and (dWA[i][j] == 0 or dWA[i][j] == self.DYNAMIC_WINDOW or dWA[i][j] == self.FORBIDEN)):
+							dWA[i][j] = self.DYNAMIC_WINDOW
+						# Forbidden Velocities
+						elif ((v > limitV or not(wAdmissible)) and (dWA[i][j] != self.ACTUAL and dWA[i][j] != self.DYNAMIC_WINDOW)):
+							#print("V: ", v, " limitV: ", limitV, " w: ", w, " limitW: ",limitW, " distance: ", distance )
+							dWA[i][j] = self.FORBIDEN
+
+				if not obstacle:
+					distance = 30
+
+					limitV = math.sqrt(2.0 * distance * self.AMAXV)
+					limitW = math.sqrt(2.0 * distance * self.AMAXW)
+
+					wAdmissible = True
+					if limitW >= 0:
+						wAdmissible = w <= limitW
+					else:
+						wAdmissible = w >= limitW
+
+					# Feasible velocities
+					if (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp and v <= limitV and wAdmissible and dWA[i][j]!= self.ACTUAL and dWA[i][j] != self.FORBIDEN):
+						dWA[i][j] = self.FEASIBLE
+					# Dynamic Window
+					elif (v >= limitVLow and v <= limitVUp and w >= limitWLow and w <= limitWUp and (dWA[i][j] == 0 or dWA[i][j] == self.DYNAMIC_WINDOW)):
+						dWA[i][j] = self.DYNAMIC_WINDOW
+					# Forbidden Velocities
+					elif ((v > limitV or not(wAdmissible)) and (dWA[i][j] != self.ACTUAL and dWA[i][j] != self.DYNAMIC_WINDOW)):
+						dWA[i][j] = self.FORBIDEN
+
+
 				j += 1
+
 			j = 0
 			i += 1
-			Vr1[0][1] = 1
-			Vr1[0][2] = 2
-			Vr1[0][3] = 3
-		self.matrix = Vr1
+		self.matrix = dWA
 		t2 = time.clock_gettime(time.CLOCK_REALTIME) - t1
-		return Vr, Vr1
+		#self.timeCall = t2
+		# print(t2)
+		return dWA
 	
 	def startPlot(self):
 		self.p = Process(target=self.plotDWA(), args=())
@@ -175,13 +275,12 @@ class TestNode:
 	def plotDWA(self):
 		fig, ax = plt.subplots()
 
-		y = np.arange(0.8,0, -0.1)
+		y = np.arange(0.8,-0.1, -0.1)
 		y = np.round(y, 2)
 		x = np.arange(-np.pi-(2*np.pi/7), np.pi+(2*np.pi/7), (2*np.pi/7))
 		x = np.round(x, 3)
 
 		while(True):
-			ax.cla()
 			ax.set_title("DWA")
 
 			self.lock.acquire()
@@ -191,17 +290,51 @@ class TestNode:
 			ax.set_xticklabels(x)
 			ax.set_yticklabels(y)
 
+			fig.set_size_inches(10, 10, forward=True)
 			fig.show()
 			plt.pause(0.1)
 
 	def searchVelocities(self, desired_v: float, desired_w: float, velocities):
+		l = len(velocities)
+		desired_w = 0 if desired_w == 0.0 else desired_w
 
-		pass
+		vx = desired_v
+		wx = desired_w
+		found, positive, negative, zero = False, False, False, False
+		while(not found):
+			try:
+				ind = velocities.index([vx, wx])
+				found = True
+			except:
+				if wx > -3.14 and not negative:
+					wx -= round( 5 * math.pi /180.0, 3)
+				elif wx == -3.14 and not positive:
+					negative = True
+					wx = desired_w + round(5 * math.pi/180.0, 3)
+				elif wx < 3.14 and not positive:
+					wx += round(5 * math.pi/180, 3)
+				elif wx == 3.14 and not negative:
+					positive = True
+					wx = desired_w
+				elif positive and negative and vx > 0:
+					vx -= 0.05
+					vx = round(vx, 2)
+				elif positive and negative and vx == 0:
+					zero = True
+					vx = desired_v + 0.05
+				elif positive and negative and zero:
+					vx += 0.05
+
+
+		return velocities[ind][0], velocities[ind][1]
 		
 
 	def callback(self, scan: LaserScan, odom: Odometry, desired_vel: TwistStamped):
 		x = odom.pose.pose.position.x
 		y = odom.pose.pose.position.y
+		th = odom.pose.pose.orientation.z
+
+		self.locRobot = np.array([x, y, th])
 		# Desired Velocities
 		desired_v = desired_vel.twist.linear.x																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
 		desired_w = desired_vel.twist.angular.z
@@ -215,6 +348,15 @@ class TestNode:
 		top = np.array(scan_ranges[601:])
 		bottom = np.array(scan_ranges[:477])
 
+		distanceAngles = 2.5
+
+		th = np.arange(-self.ANGLE_VISION/2, self.ANGLE_VISION/2 + distanceAngles, distanceAngles)
+		 
+
+		scan_sub = np.array([[np.amin(scan_ranges[(k-10):k]), th[int((k/10) - 1)]] for k in np.arange(10, len(scan_ranges) + 19, 10)])
+
+		scan_sub = np.array([[s[0], np.deg2rad(s[1])] for s in scan_sub])
+
 		# ¿Pongo las velocidades máximas o las deseadas?
 		vMin = 0.0
 		wMin = -math.pi
@@ -225,9 +367,10 @@ class TestNode:
 		# Vs = np.array([[0 for w in np.arange(wMin, wMax, math.pi/180.0)] for v in np.arange(vMin, vMax, 0.01)])
 		
 
-		_,Vr = TestNode.velocities(self, middle, top, bottom, vAct, wAct, vMin, vMax, wMin, wMax)
+		arrV= TestNode.velocities(self, vAct, wAct, vMin, vMax, wMin, wMax, scan_sub)
 
-		self.matrix = Vr
+
+		TestNode.searchVelocities(self, desired_v, desired_w, arrV)
 
 		# v, w = TestNode.skipObstacle(self,scan_ranges, middle, top, bottom, desired_v, desired_w, distance)
 		
